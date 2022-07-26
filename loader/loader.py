23536988ae15688fbfe1217f569da0935cd3117b
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-25 22:03:31 -0500

    update

--- loader/loader.py
+++ loader/loader.py
@@ -5,10 +5,15 @@ from torch.utils.data import DataLoader
 from params.params import params_dict
 
 
-def load_mnist():
+def load_mnist(dataset_name):
+    if dataset_name == "mnist":
+        load_func = datasets.MNIST
+    else:
+        raise ValueError("Invalid dataset name")
+
     batch_size = params_dict["batch_size"]
     img_transform = transforms.Compose([transforms.ToTensor()])
-    data = datasets.MNIST(DATA_PATH, download=True, train=True, transform=img_transform)
+    data = load_func(DATA_PATH, download=True, train=True, transform=img_transform)
     data_loader = DataLoader(data, batch_size=batch_size, shuffle=True)
 
     return data_loader
--- params/params.py
+++ params/params.py
@@ -1,1 +1,2 @@
-params_dict = {"channel": 64, "latent_dim": 2, "epoch": 50, "lr": 0.001, "batch_size":128, "beta": 1}
+params_dict = {"channel": 64, "latent_dim": 2, "epoch": 50, "lr": 0.001, "batch_size": 128, "beta": 1}
+size = 28
