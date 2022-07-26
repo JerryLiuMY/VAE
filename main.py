3707b1190924cc0757464d59baac2beb0e1244b0
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-26 01:50:18 -0500

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from loader.loader import load_data
 from models.train import train_vae
 from models.train import valid_vae
 from global_settings import OUTPUT_PATH
+from visualization.visualize import plot_output
 import numpy as np
 import torch
 import os
@@ -25,6 +26,24 @@ def experiment(dataset):
     torch.save(model, os.path.join(model_path, "model.pth"))
     np.save(os.path.join(model_path, "train_loss.npy"), train_loss)
     np.save(os.path.join(model_path, "valid_loss.npy"), valid_loss)
+
+
+def visualize(dataset):
+    """ Perform various visualizations
+    :param dataset: dataset name
+    """
+
+    # load data and model
+    model_path = os.path.join(OUTPUT_PATH, "model")
+    model = torch.load(os.path.join(model_path, "model.pth"), map_location=torch.device("cpu"))
+    train_loader, valid_loader, input_shape = load_data(dataset)
+    images, labels = next(iter(valid_loader))
+
+    # plot visualizations
+    visual_path = os.path.join(OUTPUT_PATH, "visual")
+    plot_output(model, images)
+
+    pass
 
 
 if __name__ == "__main__":
--- visualization/visualize.py
+++ visualization/visualize.py
@@ -1,36 +1,39 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import torchvision.utils
+import matplotlib.pyplot as plt
 from global_settings import device
 from tools.utils import to_img
+from global_settings import OUTPUT_PATH
+import numpy as np
+import os
 import torch
 plt.ion()
 
 
-def visualise_output(model, images):
+def plot_output(model, images):
     """ Visualize original and reconstructed images
-    :param model:
-    :param images:
+    :param model: trained vae model
+    :param images: images
     :return:
     """
 
     # original images
-    show_image(torchvision.utils.make_grid(images[1:50], 10, 5))
-    plt.show()
+    images_org = to_img(images)
+    images_org = torchvision.utils.make_grid(images_org[0:50], 10, 5).numpy()
+    images_org = np.transpose(images_org, (1, 2, 0))
 
     # reconstructed images
     model.eval()
     with torch.no_grad():
-        images = images.to(device)
-        images, _, _ = model(images)
-        images = images.cpu()
-        images = to_img(images)
-        np_imagegrid = torchvision.utils.make_grid(images[1:50], 10, 5).numpy()
-        plt.imshow(np.transpose(np_imagegrid, (1, 2, 0)))
-        plt.show()
+        images_rec = images.to(device)
+        images_rec, _, _ = model(images_rec)
+        images_rec = images_rec.cpu()
+        images_rec = to_img(images_rec)
+        images_rec = torchvision.utils.make_grid(images_rec[0:50], 10, 5).numpy()
+        images_rec = np.transpose(images_rec, (1, 2, 0))
 
-
-def show_image(img):
-    img = to_img(img)
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    # reconstructed images
+    fig, axes = plt.subplots(2, 1, figsize=(12, 10))
+    axes[0].imshow(images_org)
+    axes[1].imshow(images_rec)
+    visual_path = os.path.join(OUTPUT_PATH, "visual")
+    fig.savefig(os.path.join(visual_path, "visual"))
