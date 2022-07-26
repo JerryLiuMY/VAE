6938cf85a457b95eee4f173ad065db2b0e115645
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-07-26 11:53:55 -0500

    Update utils.py

--- tools/utils.py
+++ tools/utils.py
@@ -1,9 +1,43 @@
-def to_img(x):
-    """ This function takes as an input the
-    :param x: reconstructed image
-    :return:
-    """
+import warnings
 
-    x = x.clamp(0, 1)
 
-    return x
+def ignore_warnings(func):
+    """ Ignore warnings """
+
+    def inner(*args, **kwargs):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return func(*args, **kwargs)
+
+    return inner
+
+
+def iterable_wrapper(func):
+    """ Wrap generator to be reusable """
+    def inner(*args, **kwargs):
+        return IterableWrapper(func, *args, **kwargs)
+    return inner
+
+
+class IterableWrapper:
+    """ Wrap generator to be reusable """
+    def __init__(self, func, *args, **kwargs):
+        self.func, self.args, self.kwargs = func, args, kwargs
+        self.generator = self.func(*self.args, **self.kwargs)
+
+    def __iter__(self):
+        self.generator = self.func(*self.args, **self.kwargs)
+        return self
+
+    def __len__(self):
+        length = 0
+        for _ in self.generator:
+            length += 1
+        return length
+
+    def __next__(self):
+        item = next(self.generator)
+        if item is not None:
+            return item
+        else:
+            raise StopIteration
