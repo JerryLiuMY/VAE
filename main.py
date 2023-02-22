7c9bca98a1fa954cd8fe024f1d4168cbedc2656d
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2023-02-21 22:32:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def experiment(dataset):
     :param dataset: dataset name
     """
 
-    # load data and perform training
-    train_loader, valid_loader, input_shape = load_data(dataset)
-    model, train_loss = train_vae(train_loader, input_shape)
-    valid_loss = valid_vae(model, valid_loader)
-
     # save model and loss
     model_path = os.path.join(OUTPUT_PATH, "model")
     if not os.path.isdir(model_path):
         os.mkdir(model_path)
+    
+    # load data and perform training
+    train_loader, valid_loader, input_shape = load_data(dataset)
+    model, train_loss = train_vae(train_loader, input_shape)
+    valid_loss = valid_vae(model, valid_loader)
 
     torch.save(model, os.path.join(model_path, "model.pth"))
     np.save(os.path.join(model_path, "train_loss.npy"), train_loss)
